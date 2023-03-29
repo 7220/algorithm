12cb5f618101fd43b24cc7b18be53462daea07d7
7220 <kumasaki@naver.com>
2023-03-30 00:29:20 +0900

    gold add

--- test1.js
+++ test1.js
@@ -1,16 +1,3 @@
 const a = (require('fs').readFileSync('input.txt')+'').trim().split('\r\n')
-let sum = 0
-let o = 0
-for (let i = 1; i <= +a[0]; i++) {
-  const b = a[i].split('')
-  for (let j = 0; j < b.length; j++ ) {
-    if (b[j] === 'O') {
-      o++
-      sum += o
-    } else {
-      o = 0
-    }
-  }
-  console.log(sum)
-  sum = 0
-}
+
+for()
